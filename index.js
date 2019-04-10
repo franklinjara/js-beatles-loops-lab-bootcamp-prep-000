9a7f1c1a26d9dcb02ec6ad481371af88fed20982
Franklin <makyfj@hotmail.com>
2019-04-10 18:22:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
 }
 
 function johnLennonFacts(arrayOfFacts) {
-  const
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
 }
